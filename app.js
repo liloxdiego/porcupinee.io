841d7217b05a8f4f3ea8bbe7da9f830d1b2fd541
Eduardo <edwardoyarzun@gmail.com>
2014-04-22 23:26:23 -0300

    Added translation support

--- app.js
+++ app.js
@@ -4,33 +4,59 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+	routes = require('./routes'),
+	user = require('./routes/user'),
+	http = require('http'),
+	path = require('path'),
+	i18n = require("i18n"),
+	app = express();
 
-var app = express();
+i18n.configure({
+	locales : ["en", "es"],
+	directory : __dirname + '/locale',
+	cookie : "language"
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(express.cookieParser());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(i18n.init);
+// Should be always before app.router
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get("/lang/en", function(req, res) {
+	res.cookie('language', 'en', {
+		maxAge : 900000,
+		httpOnly : true
+	});
+	// routes.index(req, res);
+	res.redirect("/");
 });
+app.get("/lang/es", function(req, res) {
+	res.cookie('language', 'es', {
+		maxAge : 900000,
+		httpOnly : true
+	});
+	// routes.index(req, res);
+	res.redirect("/");
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Porcupinee' });
+exports.index = function(req, res) {
+	res.render('index', {
+		title : 'Porcupinee'
+	});
 };
